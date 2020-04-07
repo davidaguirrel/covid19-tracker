cad84aee5de4813b13de8fa0fc7cbc7a8de8f4b5
David's MAC <dal1986@gmail.com>
2020-04-07 18:23:38 -0500

    Updates

---
+++ precache-manifest.dd6e67ba5b2b98b9c28f0cd98436d945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b304aedad284978a9681e871624337c0",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "ca971696c11860fe1fba",
+    "url": "/covid19-tracker/static/css/main.5aeac134.chunk.css"
+  },
+  {
+    "revision": "708496821099d71422a8",
+    "url": "/covid19-tracker/static/js/2.339a4766.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-tracker/static/js/2.339a4766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca971696c11860fe1fba",
+    "url": "/covid19-tracker/static/js/main.5aa0b3bc.chunk.js"
+  },
+  {
+    "revision": "dbae980a6809b37e52bb",
+    "url": "/covid19-tracker/static/js/runtime-main.56e53f11.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/covid19.d7265326.png"
+  }
+]);
